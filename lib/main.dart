7ffca5e1c095e955d827bf19e34820bb80083a1d
nagaba <trevornagaba@gmail.com>
2020-06-22 03:24:37 +0300

    Add base layout

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: HomePage(), // becomes the route named '/'
+    routes: <String, WidgetBuilder> {
+      '/cause': (BuildContext context) => CausePage(),
+    },));
 }
 
 final dummyUserSnapshot = [
@@ -72,46 +76,108 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Baby Names',
-      home: MyHomePage(),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  _MyHomePageState createState() => _MyHomePageState();
+class HomePage extends StatefulWidget {
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Penda")),
       body: _buildBody(context),
     );
   }
+
+  Widget _buildBody(BuildContext context) {
+    return _buildList(context, dummyUserSnapshot, dummyCauseSnapshot);
+  }
+
+  Widget _buildList(
+      BuildContext context, List<Map> userSnapshot, List<Map> causeSnapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 0.0),
+      children:
+          causeSnapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, Map data) {
+    return Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Card(
+            margin: EdgeInsets.all(10),
+            child: Column(children: <Widget>[
+              Image.asset(
+                'images/lake.jpg',
+                width: 600,
+                height: 240,
+                fit: BoxFit.cover,
+              ),
+              Row(children: <Widget>[
+                Container(
+                    padding: const EdgeInsets.all(15.0),
+                    child: (Text(
+                      'dummy title text here',
+                    ))),
+                Container(
+                    padding: const EdgeInsets.only(left: 65.0),
+                    child: Text('UGX 100,000'))
+              ]),
+              Row(children: <Widget>[
+                Container(
+                    padding: const EdgeInsets.only(left: 15.0, right: 60),
+                    // TO-DO
+                    // make padding screenresponsive
+                    child: Text(
+                      'dummy description text here',
+                      // TO-DO
+                      // find a way to wrap the description
+                      overflow: TextOverflow.ellipsis,
+                      softWrap:
+                          true, // text lines will fill the column width before wrapping at a word boundary
+                    )),
+                IconButton(
+                    icon: Icon(Icons.share),
+                    alignment: Alignment.centerRight,
+                    onPressed: () {
+                      Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text("SnackBar"),
+                      ));
+                    })
+              ]),
+              Row(children: <Widget>[
+                Container(
+                    padding: const EdgeInsets.only(left: 7.0, right: 87.0),
+                    child: ButtonBar(children: [
+                      // TO-DO
+                      // Add the action/route for the on_pressed method
+                      RaisedButton(
+                        child: Text('expand'),
+                        onPressed: () {Navigator.pushNamed(context, '/cause');}, // Add the navigator to go to a cause
+                      )
+                    ], alignment: MainAxisAlignment.start)),
+                Text('due date: 31/12/2020')
+              ])
+            ])));
+  }
 }
 
-Widget _buildBody(BuildContext context) {
-  return _buildList(context, dummyUserSnapshot, dummyCauseSnapshot);
+class CausePage extends StatefulWidget {
+  _CausePageState createState() => _CausePageState();
 }
 
-Widget _buildList(
-    BuildContext context, List<Map> userSnapshot, List<Map> causeSnapshot) {
-  return ListView(
-    padding: const EdgeInsets.only(top: 20.0),
-    children:
-        causeSnapshot.map((data) => _buildListItem(context, data)).toList(),
-  );
-}
-
-Widget _buildListItem(BuildContext context, Map data) {
-  return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
-            borderRadius: BorderRadius.circular(5.0),
-          ),
-          child: Card(
-              margin: EdgeInsets.all(10),
-              child: Column(children: <Widget>[Text('Dummy text here')]))));
+class _CausePageState extends State<CausePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Cause details")),
+      // body: _buildBody(context), // TO-DO
+    );
+  }
 }
